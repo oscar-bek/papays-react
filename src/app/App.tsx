141d60b3bc17b1fed84e27d473af0d2f09317dcd
oscar-bek <salamov.behzod@gmail.com>
2022-10-22 14:46:20 +0900

    feat: standart integration og MUI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,20 @@
+import { Box, Button, Container, Stack, Typography } from '@mui/material';
+
 import React from 'react';
 import '../css/App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Container maxWidth="sm">
+      <Stack flexDirection={"column"}>
+        <Box sx={{my: 4}}>
+          <Typography variant='h4' component={'h1'} gutterBottom>
+            Create React App on Typescrypt with REDUX
+          </Typography>
+        </Box>
+        <Button variant="contained">Contained</Button>
+      </Stack>
+    </Container>
   );
 }
 

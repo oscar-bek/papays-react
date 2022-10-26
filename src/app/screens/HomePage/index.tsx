13128b76ae37445e0eb6eb84557b53ca5ef5c15c
oscar-bek <salamov.behzod@gmail.com>
2022-10-26 23:49:43 +0900

    feat: develop homepage statistics component

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import { Container } from "@mui/system";
+import { Statistics } from "./statistics";
+import { TopRestaurants } from "./topRestaurants";
+import { BestRestaurants } from "./bestRestaurants";
+import { BestDishes } from "./bestDishes";
+import { Advertisements } from "./advertisements";
+import { Recommendation } from "./recommendation";
+import { Events } from "./events";
+import '../../../css/home.css';
 
 export function HomePage() {
 return (
-  <Container>HomePage</Container>
+  <div className="homepage">
+    <Statistics/>
+    <TopRestaurants/>
+    <BestRestaurants/>
+    <BestDishes/>
+    <Advertisements/>
+    <Events/>
+    <Recommendation/>
+  </div>
   )
 }

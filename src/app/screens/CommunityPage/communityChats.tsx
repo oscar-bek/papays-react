6e51c86dc086623d3c68c1929b56b33bafb81258
oscar-bek <salamov.behzod@gmail.com>
2023-01-17 22:11:44 +0900

    feat: connecting clinet socket to backend socket and develop socket.io client fundamental logic

--- src/app/screens/CommunityPage/communityChats.tsx
+++ src/app/screens/CommunityPage/communityChats.tsx
@@ -1,14 +1,43 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Avatar, Box, Stack } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import { SocketContext } from "../../context/socket";
 
 export function CommunityChats() {
   /** INITIALIZATIONS **/
   const [messagesList, setMessagesList] = useState([]);
+  const socket = useContext(SocketContext);
+  const [onlineUsers, setOnlineUsers] = useState<number>(0);
+
+  useEffect(() => {
+    socket.connect();
+    console.log("PRINTED");
+
+    socket?.on("connect", function () {
+      console.log("CLIENT: connected");
+    });
+
+    socket?.on("newMsg", (new_message: any) => {
+      console.log("CLIENT: new message");
+    });
+
+    socket?.on("greetMsg", (greet_message: any) => {
+      console.log("CLIENT: greet message");
+    });
+
+    socket?.on("infoMsg", (msg: any) => {
+      console.log("CLIENT: info message");
+      setOnlineUsers(msg.total);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [socket]);
 
   return (
     <Stack className={"chat_frame"}>
-      <Box className={"chat_top"}>Jonli Muloqot</Box>
+      <Box className={"chat_top"}>Jonli Muloqot { onlineUsers }</Box>
       <Box className={"chat_content"}>
         <Stack className={"chat_main"}>
           <Box
